1fe5d95967e415a26e41c382d06456c82323cf75
Stephen Akuffo <sa.akuffo@gmail.com>
2022-01-30 20:34:06 -0500

    finished

--- chap1/exercises/1_my_guessing_game.py
+++ chap1/exercises/1_my_guessing_game.py
@@ -1,21 +1,30 @@
 import random
 from time import sleep
 
+# write a function(guessing_game) that takes no arguments
+
 
 def guessing_game():
     '''
       A Simple Guessing Game
     '''
+    # when run, the function chooses a random integer between 0 and 100(inclusive)
     correct_num = random.randint(0, 100)
 
+    # Ask the user to guess what number has been chosen
     while True:
-        user_num = int(input("Please Guess a Number between 1 and 100: "))
+        user_guess = input("Please Guess a Number between 1 and 100: ")
+        user_num = int(user_guess)
         if not_valid_number(user_num):
             continue
+        # If higher print out "Too High"
         elif user_num > correct_num:
             print("Too High!")
+        # If lower print out "Too Low"
         elif user_num < correct_num:
             print("Too Low!")
+        # If correct print out "Just Right"
+        # the program exits if the user guesses correctly
         elif user_num == correct_num:
             print("Just Right!")
             break
